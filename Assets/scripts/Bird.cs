af31bb1b657ec558eab20abd802a4b171ae48a73
ZanY22 <benediktas.dap@gmail.com>
2023-10-15 10:37:53 +0300

    idk

---
+++ Assets/scripts/Bird.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    public float jumpSpeed = 5;
+    Rigidbody2D rb;
+    public int score;
+
+    public TMP_Text scoreText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * 3f);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+    public void Die()
+    {
+        var currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
